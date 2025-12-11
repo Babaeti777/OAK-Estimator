18a6a980b2c455f9c0389c252f49a2eeff091082
Babaeti777 <hish8766@gmail.com>
2025-12-11 16:03:45 -0500

    Update Firebase configuration with new credentials

--- firebase/config.js
+++ firebase/config.js
@@ -29,12 +29,12 @@
  * @type {FirebaseConfig}
  */
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY_HERE",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyD3_zQVowO0Sg5rFSGkcSU0NoI852PuPAA",
+  authDomain: "construction-estimator-d2633.firebaseapp.com",
+  projectId: "construction-estimator-d2633",
+  storageBucket: "construction-estimator-d2633.firebasestorage.app",
+  messagingSenderId: "252394591641",
+  appId: "1:252394591641:web:07d03f267c49c0007daf08"
 };
 
 /**
